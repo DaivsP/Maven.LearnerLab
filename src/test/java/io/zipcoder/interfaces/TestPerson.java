5842fa5e4af20d0405e76190a2854784b0940d34
Davis <davisperchik@gmail.com>
2019-03-05 16:09:15 -0500

    created student class. Checkpoint in work

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,35 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void constructorTest(){
+        //Given
+        Person person = new Person(15);
+
+        //When
+        Long actual = person.getId();
+
+        //Then
+        Long expected = (long)15;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNameTest(){
+        //Given
+        Person person = new Person(15);
+        person.setName("Davis");
+
+        //When
+        String actual = person.getName();
+
+        //Then
+        String expected = "Davis";
+        Assert.assertEquals(expected, actual);
+    }
 
 }
