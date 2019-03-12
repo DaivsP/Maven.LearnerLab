10c94c9de3b7fa358fd37c2977cd890866558738
Davis <davisperchik@gmail.com>
2019-03-12 18:39:51 -0400

    working on part 10

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -3,14 +3,14 @@ package io.zipcoder.interfaces;
 import java.util.ArrayList;
 import java.util.List;
 
-public class People {
-    List<Person> personList;
+public abstract class People<E extends Person> {
+    List<E> personList;
 
     public People(){
-        this.personList = new ArrayList<Person>();
+        this.personList = new ArrayList<E>();
     }
 
-    public void add(Person person) {
+    public void add(E person) {
         personList.add(person);
     }
 
@@ -18,13 +18,13 @@ public class People {
         return personList.size();
     }
 
-    public void remove(Person person) {
+    public void remove(E person) {
         personList.remove(person);
     }
 
     public Person getById(long id) {
-        Person returnPerson = null;
-        for (Person person : personList) {
+        E returnPerson = null;
+        for (E person : personList) {
             if (person.getId() == id){
                 returnPerson = person;
             }
@@ -33,7 +33,7 @@ public class People {
     }
 
     public void remove(long id){
-        for (Person person : personList) {
+        for (E person : personList) {
             if (person.getId() == id){
                 personList.remove(person);
                 break;
@@ -41,9 +41,7 @@ public class People {
         }
     }
 
-    public Person[] getArray(){
-        return personList.toArray(new Person[0]);
-    }
+    public abstract E[] getArray();
 
     public void removeAll(){
         personList.clear();
